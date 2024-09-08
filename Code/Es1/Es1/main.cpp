023e4f6c4183f147873583cce0b17fc2173617fd
Stefano Tonini <72923946+Grandediw@users.noreply.github.com>
2024-09-08 19:02:44 +0200

    Add files via upload

---
+++ Code/Es1/Es1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Es1
+//
+//  Created by Stefano Tonini on 31/01/21.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x=1;
+    double y=2;
+    cout<<x/y<<endl;
+}
